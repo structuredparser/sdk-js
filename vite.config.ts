252f6c420d03993ca3713af879524d6a7297a332
Lazhar <hello@lazharichir.com>
2025-01-21 12:23:30 +0800

    fix: remove commented globals option in Vitest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 /// <reference types="vitest" />
-
 // Configure Vitest (https://vitest.dev/config/)
 
 import { defineConfig, loadEnv } from "vite"
 
 export default defineConfig({
   test: {
-    /* for example, use global to avoid globals imports (describe, test, expect): */
-    // globals: true,
     setupFiles: ["vitest.setup.ts"],
     dangerouslyIgnoreUnhandledErrors: true,
     env: loadEnv("test.local", process.cwd(), ""),
